744f955436281e05a9b140c263f87f88d36ef644
mostfaswaisi93 <mostfaswaisi@gmail.com>
2020-05-03 18:56:43 +0300

    Make Models and Controllers - DB

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,18 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+// dropzone routes
+
+Route::get('/posts', 'PostController@index')->name('posts.index');
+Route::get('/posts/{post}', 'PostController@show')->name('posts.show');
+
+Route::post('/posts/{post}', 'PostController@upload')->name('posts.upload');
+
+Route::delete('/images/{image}', 'ImageController@destroy')->name('images.destroy');
